785e7ba019a19379537c2c971533666c87a09620
Gagandeep Samra <gsamra890@github.com>
2021-03-29 19:54:50 -0400

    Final W8 - Set Up A Back End

--- public/scripts.js
+++ public/scripts.js
@@ -1,35 +1,21 @@
-async function populateRestaurant() {
+async function dinnerHall() {
   const diningRequest = await fetch("/api/dining");
   const diningData = await diningRequest.json();
-  console.table(diningData);
-  const dining_id = document.querySelector(".dining_id");
-  const name = document.querySelector(".name");
-  const address = document.querySelector(".address");
-  
+  const diner = diningData.data;
+  const check = document.querySelector(".target");
 
-  diningData.data.forEach((item) => {
-    const appendID = document.createElement("ul");
-    //appendID.classList.add("title", "has-text-centered", "is-parent", "is-3");
-    appendID.classList('.dining_id')
-    appendID.innerHTML = ` ${item.hall_id}`;
-    dining_id.append(appendID)
-
-    const appendName = document.createElement("ul");
-    //appendName.classList.add("title", "has-text-centered", "is-parent", "is-3");
-    appendName.classList('.name')
-    appendName.innerHTML = ` ${item.hall_name}`;
-    name.append(appendName)
-
-    const appendAddres = document.createElement("ul");
-    //appendAddres.classList.add("title", "has-text-centered", "is-parent", "is-3");
-    appendAddres.classList('.address')
-    appendAddres.innerHTML = ` ${item.hall_address.split(',')[0]}`;
-    address.append(appendAddres)
+  diner.forEach((element) => {
+    const checkEelement = document.createElement("tr");
+    checkEelement.innerHTML = `
+   <td>${element.hall_id}</td>
+    <td>${element.hall_name}</td>
+    <td>${element.hall_address}</td>`;
+    check.append(checkEelement);
   });
 }
 
 async function windowActions() {
-  populateRestaurant();
+  dinnerHall();
 }
 
 window.onload = windowActions();
